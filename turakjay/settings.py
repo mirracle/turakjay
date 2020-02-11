269977ab2876ae7e10ba6371e7f64133efe084e1
jyldyz <dushebekov@mail.ru>
2020-02-11 20:31:02 +0600

    cors 2

--- turakjay/settings.py
+++ turakjay/settings.py
@@ -53,11 +53,13 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'drf_yasg',
     'phonenumber_field',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
